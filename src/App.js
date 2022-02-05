a58d552ed08633d6a28d0fbff122d0aaf048d5a2
subarna <subarna769@gmail.com>
2022-02-06 01:47:22 +0600

    fix: babel elstin and antd desing and prettier error solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import { Layout } from 'antd';
 import React from 'react';
 import './App.css';
-import AppFooter from './layout/footer';
-import AppHeader from './layout/header';
+
+
 
 const { Content } = Layout;
-
 
 function App() {
     return (
         <div className="App">
-            <Layout className="layout">
-                <AppHeader />
-                 <Content>
-                    <div className="site-layout-content">
-                        <Home />
-                    </div>
-                </Content>
-                <AppFooter />
-            </Layout>
+            <Content>
+                <h1>refwererfd</h1>
+            </Content>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <HeroSection />
+     <h1>fowejkdrfe</h1>
     </div>
   );
 }
